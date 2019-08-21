f8e9fb9e6b1345aab54ab2707051a34b73a77d6c
johnny chan <johchan24@gmail.com>
2019-08-22 01:00:09 +1000

    components added

---
+++ src/components/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: ["14v2j9FG5lXpMA", "10hzvF9FTulLxK"],
+      selectedGif: "14v2j9FG5lXpMA"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGif} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
